81af85911cf833010f41ec91ea8aa49c2e353f04
GUSSAN <gussan.beedan@gmail.com>
2023-04-06 08:54:55 +0900

    条件分岐を追加して、admin-second/admin-third管理テーマいずれにも対応。

--- View/Elements/admin/banner_areas/index_list.php
+++ View/Elements/admin/banner_areas/index_list.php
@@ -11,6 +11,7 @@
 <!-- pagination -->
 <?php $this->BcBaser->element('pagination') ?>
 
+<?php if($this->BcBaser->siteConfig['admin_theme'] == 'admin-third'): ?>
 <table cellpadding="0" cellspacing="0" class="list-table sort-table bca-table-listup" id="ListTable">
     <thead class="bca-table-listup__thead">
         <tr>
@@ -69,9 +70,65 @@
             <tr>
                 <td colspan="6"><p class="no-data">データがありません。</p></td>
             </tr>
-        <?php endif; ?>
+        <?php endif ?>
     </tbody>
 </table>
-
+<?php else: ?>
+<table cellpadding="0" cellspacing="0" class="list-table sort-table" id="ListTable">
+    <thead>
+        <tr>
+            <th style="width: 50px" class="list-tool">
+                <div>
+                    <?php $this->BcBaser->link($this->BcBaser->getImg('admin/btn_add.png', array('alt' => '新規追加', 'class' => 'btn')). __d('baser', '新規追加'), array('action' => 'add')) ?>
+                </div>
+            </th>
+            <th><?php echo $this->Paginator->sort('id', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' NO',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' NO'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+            </th>
+            <th>
+                <?php echo $this->Paginator->sort('name', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' 表示場所',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' 表示場所'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+            </th>
+            <th>
+                <?php echo $this->Paginator->sort('width', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' 横幅',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' 横幅'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+            </th>
+            <th>
+                <?php echo $this->Paginator->sort('height', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' 高さ',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' 高さ'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+            </th>
+            <th><?php echo $this->Paginator->sort('created', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' 登録日',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' 登録日'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+                <br />
+                <?php echo $this->Paginator->sort('modified', array(
+                    'asc' => $this->BcBaser->getImg('admin/blt_list_down.png', array('alt' => '昇順', 'title' => '昇順')).' 更新日',
+                    'desc' => $this->BcBaser->getImg('admin/blt_list_up.png', array('alt' => '降順', 'title' => '降順')).' 更新日'),
+                    array('escape' => false, 'class' => 'btn-direction')) ?>
+            </th>
+        </tr>
+    </thead>
+<tbody>
+<?php if(!empty($datas)): ?>
+    <?php foreach($datas as $data): ?>
+        <?php $this->BcBaser->element('banner_areas/index_row', array('data' => $data)) ?>
+    <?php endforeach; ?>
+<?php else: ?>
+    <tr>
+        <td colspan="6"><p class="no-data">データがありません。</p></td>
+    </tr>
+<?php endif ?>
+    </tbody>
+</table>
+<?php endif ?>
 <!-- list-num -->
 <?php $this->BcBaser->element('list_num') ?>
