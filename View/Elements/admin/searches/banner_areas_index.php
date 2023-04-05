5751a44fa799346a30fefcf3b9c320014e99191f
GUSSAN <gussan.beedan@gmail.com>
2023-04-05 19:29:33 +0900

    admin-third対応化クラスの追加、ほか。

--- View/Elements/admin/searches/banner_areas_index.php
+++ View/Elements/admin/searches/banner_areas_index.php
@@ -2,20 +2,37 @@
 /**
  * [BANNER] バナーエリア管理
  *
- * @copyright		Copyright 2014 - 2018, D-ZERO Co.,LTD.
- * @link			http://www.d-zero.co.jp/
- * @package			Banner
- * @license			MIT
+ * @copyright       Copyright 2014 - 2018, D-ZERO Co.,LTD.
+ * @link            http://www.d-zero.co.jp/
+ * @package         Banner
+ * @license         MIT
  */
 ?>
 <?php echo $this->BcForm->create('BannerArea', array('url' => array('action' => 'index'))) ?>
-<p>
-	<span>
-		<?php echo $this->BcForm->label('BannerArea.name', '表示場所') ?>
-		&nbsp;<?php echo $this->BcForm->input('BannerArea.name', array('type' => 'text', 'size' => '30')) ?>
-	</span>
+<p class="bca-search__input-list">
+    <span class="bca-search__input-item">
+        <?php echo $this->BcForm->label('BannerArea.name', '表示場所', ['class' => 'bca-search__input-item-label']) ?>
+        &nbsp;<?php echo $this->BcForm->input('BannerArea.name', ['type' => 'text', 'size' => '30']) ?>
+    </span>
 </p>
-<div class="button">
-	<?php echo $this->BcForm->submit('admin/btn_search.png', array('alt' => '検索', 'class' => 'btn'), array('div' => false, 'id' => 'BtnSearchSubmit')) ?>
+<div class="button bca-search__btns">
+    <div class="bca-search__btns-item">
+        <?php $this->BcBaser->link(
+            __d('baser', '検索'), 
+            "javascript:void(0)", 
+            ['id' => 'BtnSearchSubmit', 
+                'class' => 'bca-btn', 
+                'data-bca-btn-type' => 'search'
+        ]) ?>
+    </div>
+    <div class="bca-search__btns-item">
+        <?php $this->BcBaser->link(
+            __d('baser', 'クリア'), 
+            "javascript:void(0)", 
+            ['id' => 'BtnSearchClear', 
+                'class' => 'bca-btn', 
+                'data-bca-btn-type' => 'clear'
+        ]) ?>
+    </div>
 </div>
 <?php echo $this->BcForm->end() ?>
--- View/Elements/admin/searches/banner_files_index.php
+++ View/Elements/admin/searches/banner_files_index.php
@@ -9,32 +9,49 @@
  */
 ?>
 <?php echo $this->BcForm->create('BannerFile', array('url' => array('action'=>'index', $bannerArea['BannerArea']['id']))) ?>
-<p>
-	<span style="margin-right: 20px;">
-		<?php echo $this->BcForm->label('BannerFile.name', 'ファイル名') ?>
+<p class="bca-search__input-list">
+	<span class="bca-search__input-item" style="margin-right: 20px;">
+		<?php echo $this->BcForm->label('BannerFile.name', 'ファイル名', ['class' => 'bca-search__input-item-label']) ?>
 		&nbsp;<?php echo $this->BcForm->input('BannerFile.name', array('type' => 'text', 'size' => '20')) ?>
 	</span>
-	<span>
-		<?php echo $this->BcForm->label('BannerFile.alt', 'alt') ?>
+	<span class="bca-search__input-item">
+		<?php echo $this->BcForm->label('BannerFile.alt', 'alt', ['class' => 'bca-search__input-item-label']) ?>
 		&nbsp;<?php echo $this->BcForm->input('BannerFile.alt', array('type' => 'text', 'size' => '20')) ?>
 	</span>
 </p>
-<p>
-	<span style="margin-right: 20px;">
-		<?php echo $this->BcForm->label('BannerFile.url', 'リンクURL') ?>
+<p class="bca-search__input-list">
+	<span class="bca-search__input-item" style="margin-right: 20px;">
+		<?php echo $this->BcForm->label('BannerFile.url', 'リンクURL', ['class' => 'bca-search__input-item-label']) ?>
 		&nbsp;<?php echo $this->BcForm->input('BannerFile.url', array('type' => 'text', 'size' => '20')) ?>
 	</span>
-	<span style="margin-right: 20px;">
-		<?php echo $this->BcForm->label('BannerFile.status', '公開状態') ?>
+	<span class="bca-search__input-item" style="margin-right: 20px;">
+		<?php echo $this->BcForm->label('BannerFile.status', '公開状態', ['class' => 'bca-search__input-item-label']) ?>
 		&nbsp;<?php echo $this->BcForm->input('BannerFile.status', array('type' => 'select', 'options' => $this->BcText->booleanMarkList(), 'empty' => '指定なし')) ?>
 	</span>
-	<span>
-		<?php echo $this->BcForm->label('BannerFile.blank', '別窓で開く') ?>
+	<span class="bca-search__input-item">
+		<?php echo $this->BcForm->label('BannerFile.blank', '別窓で開く', ['class' => 'bca-search__input-item-label']) ?>
 		&nbsp;<?php echo $this->BcForm->input('BannerFile.blank', array('type' => 'select', 'options' => $this->BcText->booleanMarkList(), 'empty' => '指定なし')) ?>
 	</span>
 	<?php echo $this->BcSearchBox->dispatchShowField() ?>
 </p>
-<div class="button">
-	<?php echo $this->BcForm->submit('admin/btn_search.png', array('alt' => '検索', 'class' => 'btn'), array('div' => false, 'id' => 'BtnSearchSubmit')) ?>
+<div class="button bca-search__btns">
+    <div class="bca-search__btns-item">
+        <?php $this->BcBaser->link(
+            __d('baser', '検索'), 
+            "javascript:void(0)", 
+            ['id' => 'BtnSearchSubmit', 
+                'class' => 'bca-btn', 
+                'data-bca-btn-type' => 'search'
+        ]) ?>
+    </div>
+    <div class="bca-search__btns-item">
+        <?php $this->BcBaser->link(
+            __d('baser', 'クリア'), 
+            "javascript:void(0)", 
+            ['id' => 'BtnSearchClear', 
+                'class' => 'bca-btn', 
+                'data-bca-btn-type' => 'clear'
+        ]) ?>
+    </div>
 </div>
 <?php echo $this->BcForm->end() ?>
