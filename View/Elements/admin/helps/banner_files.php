d9878f6905df6e7acb5d6ac239409368ea657895
GUSSAN <gussan.beedan@gmail.com>
2023-04-06 09:34:05 +0900

    条件分岐を追加して、admin-second/admin-third管理テーマいずれにも対応。

--- View/Elements/admin/helps/banner_files.php
+++ View/Elements/admin/helps/banner_files.php
@@ -10,10 +10,17 @@
 ?>
 <?php if($this->request->action == 'admin_index'): ?>
 <p>バナーの管理が行えます。</p>
+<?php if($this->BcBaser->siteConfig['admin_theme'] == 'admin-third'): ?>
 <ul>
-	<li>新しいバナーを登録するには、画面上の「新規バナー追加」ボタンをクリックします。</li>
-	<li>操作欄の <i class="bca-icon--edit" data-bca-btn-size="md"></i> ボタンからは、バナーの編集画面へ移動できます。</li>
+    <li>新しいバナーを登録するには、画面上の「新規バナー追加」ボタンをクリックします。</li>
+    <li>操作欄の <i class="bca-icon--edit" data-bca-btn-size="md"></i> ボタンからは、バナーの編集画面へ移動できます。</li>
 </ul>
+<?php else: ?>
+<ul>
+	<li>新しいバナーを登録するには、画面下の「新規追加」ボタンをクリックします。</li>
+	<li>操作欄の <?php $this->BcBaser->img('admin/icn_tool_edit.png') ?> ボタンからは、バナーの編集画面へ移動できます。</li>
+</ul>
+<?php endif ?>
 <?php else: ?>
 <p>バナー追加について</p>
 <ul>
